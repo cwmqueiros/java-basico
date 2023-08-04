1fef6705015a8228ff2e4b2e2c12598e39106774
carlos queiros <cwmqueiros@gmail.com>
2023-08-04 00:14:19 -0300

    Criando Conta Bancaria

---
+++ conta-bancaria/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc1 = new Scanner(System.in);
+        // Scanner scaner = new Scanner(System.in).useLocale(Locate.US);
+
+        System.out.println("Digite o número da Conta!");
+        int numero = sc1.nextInt();
+
+        System.out.println("digite o número da Agência!");
+        String agencia = sc1.next();
+
+        System.out.println("digite o Nome do Cliente!");
+        String nomeCliente = sc1.next();
+
+        System.out.println("digite o Saldo da Conta !");
+        double saldo = sc1.nextDouble();
+
+        System.out.println(
+                "Olá " + nomeCliente
+                        + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "
+                        + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+}
